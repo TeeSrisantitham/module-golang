abad899f0524a65ac8425c741fdd2eedf48e339a
Tee <tee@yelllow.com>
2020-12-10 11:03:45 +0700

    Finish ex 1

---
+++ ex01-downcase/downcase.go
@@ -0,0 +1,47 @@
+package downcase
+
+import (
+	"sync"
+	"fmt"
+)
+
+func Downcase(arg string) (string, error) {
+	length := len(arg)
+	result := make([]byte, length)
+
+	var wg sync.WaitGroup
+  wg.Add(length)
+
+	for i := 0; i < length; i++ {
+		go downcase(arg[i], result, i, &wg)
+	}
+	wg.Wait()
+	return string(result), nil
+}
+
+func downcase(arg byte, result []byte, index int, wg *sync.WaitGroup) {
+	fmt.Println(string(arg))
+	if arg >= 65 && arg <= 90 {
+		result[index] = arg + 32
+	} else {
+		result[index] = arg
+	}
+	wg.Done()
+}
+
+// func Downcase(arg string) (string, error) {
+// 	var result []byte
+// 	for i := 0; i < len(arg); i++ {
+// 		result = append(result, downcase(arg[i]))
+// 	}
+// 	return string(result), nil
+// }
+
+// func downcase(arg byte) byte {
+// 	fmt.Println(string(arg))
+// 	if arg >= 65 && arg <= 90 {
+// 		return arg + 32
+// 	} else {
+// 		return arg
+// 	}
+// }
